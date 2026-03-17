42aba7274ef8e31879313beef2490b344c7c1c78
ajxpro7 <info@wenine.nl>
2026-03-17 18:47:18 +0100

    update

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,4 +1,5 @@
 export const corsHeaders = {
+  // "Access-Control-Allow-Origin": "http://localhost:5173",
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers":
     "authorization, x-client-info, apikey, content-type",
